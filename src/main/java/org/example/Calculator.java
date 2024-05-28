349eb231f6f02792686d25302c628464956fa5e2
Sander <sanderheimans@gmail.com>
2024-05-28 11:25:31 +0200

    add method tokenizePromptToArray

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -15,7 +15,7 @@ public class Calculator {
         String prompt = input.getInput();
         try {
             int result = interpreter.interpret(prompt);
-            System.out.println("The result:" + result);
+            System.out.println("The result: " + result);
         } catch (RuntimeException e) {
             //System.out.println(e.getMessage());
             e.printStackTrace();
--- src/main/java/org/example/Interpreter.java
+++ src/main/java/org/example/Interpreter.java
@@ -2,11 +2,45 @@ package org.example;
 
 import java.util.*;
 import java.util.function.BiFunction;
-import java.util.regex.Pattern;
+
+import static java.lang.System.exit;
 
 public class Interpreter {
+    public ArrayList<String> tokenizePromptToArray(String prompt) throws Exception {
+        ArrayList<String> splitPrompt = new ArrayList<>(List.of(prompt.split("\\s+")));
+
+        boolean expectNumber = true; // start with number expected
+        String numberRegex = "-?\\d+";
+        String operatorRegex = "[+-]";
+
+        String expectedRegex;
+
+        for (String entry : splitPrompt) {
+            if (expectNumber) {
+                expectedRegex = numberRegex; // start with number expected
+                if (!entry.matches(expectedRegex)) {
+                    throw new Exception("Incorrect format: please enter whitespaces between numbers and operators");
+                }
+            } else {
+                expectedRegex = operatorRegex;
+                if (!entry.matches(expectedRegex)) {
+                    throw new Exception("Incorrect format: please enter whitespaces between numbers and operators");
+                }
+            }
+
+            expectNumber = !expectNumber;
+        }
+        return splitPrompt;
+    }
+    
     public int interpret(String prompt) {
-        ArrayList<String> splittedPrompt = new ArrayList<>(List.of(prompt.split("\\s+")));
+        ArrayList<String> splitPrompt = null; //FIXME why should this be initialized and not declared?
+        try {
+            splitPrompt = this.tokenizePromptToArray(prompt);
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+            exit(2);
+        }
 
         // Create HashMap of operators and corresponding operation
         HashMap<String, BiFunction<Integer, Integer, Integer>> operations = new HashMap<>();
@@ -17,31 +51,31 @@ public class Interpreter {
 
         // Loop over operations in order of (PE)MDAS, perform them and simplify prompt for every operator
         for (String op : operations.keySet()) {
-            for (int j = 0; j < splittedPrompt.size(); j++) {
-                if (Objects.equals(splittedPrompt.get(j), op)) {
+            for (int j = 0; j < splitPrompt.size(); j++) {
+                if (Objects.equals(splitPrompt.get(j), op)) {
 
                     // Retrieve correct operation and operands
                     BiFunction<Integer, Integer, Integer> operation = operations.get(op);
-                    Integer a = Integer.valueOf(splittedPrompt.get(j - 1));
-                    Integer b = Integer.valueOf(splittedPrompt.get(j + 1));
+                    Integer a = Integer.valueOf(splitPrompt.get(j - 1));
+                    Integer b = Integer.valueOf(splitPrompt.get(j + 1));
 
                     String subResult = String.valueOf(operation.apply(a,b));
 
                     // Update splitted prompt according to subResult
-                    splittedPrompt.set(j, subResult);
-                    splittedPrompt.remove(j + 1); // first this one to not mess with indexation
-                    splittedPrompt.remove(j - 1);
+                    splitPrompt.set(j, subResult);
+                    splitPrompt.remove(j + 1); // first this one to not mess with indexation
+                    splitPrompt.remove(j - 1);
 
                     // Adjust index as not to skip an entry
                     j--;
 
                     // Done when single number left
-                    if (splittedPrompt.size() == 1) {
+                    if (splitPrompt.size() == 1) {
                         break;
                     }
                 }
             }
         }
-        return Integer.parseInt(splittedPrompt.get(0));
+        return Integer.parseInt(splitPrompt.get(0));
     }
 }
